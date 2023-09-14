1e32acf4eebf6f5ba67168d34eecbb2653dd8e6b
Pedro Vitor Augusto de Jesus <pedrovitora.jesus@gmail.com>
2023-09-13 22:23:06 -0300

    feat: adiciona aula1exer2Evolucao3_PedroJesus_200073249

---
+++ SBD2/exercicios/aula1exer2Evolucao3_PedroJesus_200073249/aula1exer2Evolucao3_Apaga_PedroJesus_200073249.sql
@@ -0,0 +1,31 @@
+-- << aula1exer2Evolucao3 >>
+-- 
+-- 			SCRIPT DE APAGA (DDL)
+--
+-- Data Criacao ...........: 13/09/2023
+-- Autor(es) ..............: José Luís Ramos Teixeira
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: aula1exer2Evolucao3
+--
+-- PROJETO => 01 Base de Dados
+--         => 10 tabelas
+--         => 03 Perfis
+--         => 08 Usuarios
+-- 
+-- ---------------------------------------------------------
+
+USE aula1exer2Evolucao3;
+
+DROP TABLE comercializa;
+DROP TABLE possui;
+DROP TABLE telefone;
+DROP TABLE VENDA;
+DROP TABLE PRODUTO;
+DROP TABLE EMPREGADO;
+DROP TABLE FORMACAO;
+DROP TABLE GERENTE;
+DROP TABLE AREA;
+DROP TABLE PESSOA;
+
+
+
